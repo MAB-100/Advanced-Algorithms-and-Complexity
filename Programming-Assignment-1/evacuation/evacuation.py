a5c55a81242cce890f1fc8c4c7975212d0b8c725
MAB-100 <shahabbas661@gmail.com>
2024-10-03 17:57:30 +0500

    comepleted week 1, added week 2

--- Programming-Assignment-1/evacuation/evacuation.py
+++ Programming-Assignment-1/evacuation/evacuation.py
@@ -1,5 +1,8 @@
 # python3
 
+import queue
+
+
 class Edge:
 
     def __init__(self, u, v, capacity):
@@ -10,6 +13,8 @@ class Edge:
 
 # This class implements a bit unusual scheme for storing edges of the graph,
 # in order to retrieve the backward edge for a given edge quickly.
+
+
 class FlowGraph:
 
     def __init__(self, n):
@@ -47,6 +52,8 @@ class FlowGraph:
         # It turns out that id ^ 1 works for both cases. Think this through!
         self.edges[id].flow += flow
         self.edges[id ^ 1].flow -= flow
+        self.edges[id].capacity -= flow
+        self.edges[id ^ 1].capacity += flow
 
 
 def read_data():
@@ -58,9 +65,50 @@ def read_data():
     return graph
 
 
+def bfs(graph, from_, to):
+    if from_ == to:
+        return True, [], 0
+    path = []
+    parent = [(None, None) for _ in range(graph.size())]
+    distance = [float('inf') for _ in range(graph.size())]
+    min_edge_in_path = float('inf')
+    path_exists = False
+    kyu = queue.Queue()
+    kyu.put(from_)
+    distance[from_] = 0
+    while not kyu.empty():
+        currNode = kyu.get()
+        for id in graph.get_ids(currNode):
+            currEdge = graph.get_edge(id)
+            if distance[currEdge.v] == float('inf') and currEdge.capacity > 0:
+                kyu.put(currEdge.v)
+                distance[currEdge.v] = distance[currNode] + 1
+                parent[currEdge.v] = (currNode, id)
+                if currEdge.v == to:
+                    while True:
+                        path.insert(0, id)
+                        curr_edge_capacity = graph.get_edge(id).capacity
+                        min_edge_in_path = min(
+                            min_edge_in_path, curr_edge_capacity)
+                        if currNode == from_:
+                            break
+                        currNode, id = parent[currNode]
+                    path_exists = True
+                    return path_exists, path, min_edge_in_path
+    return path_exists, path, min_edge_in_path
+
+
 def max_flow(graph, from_, to):
     flow = 0
     # your code goes here
+    while True:
+        path_exists, path, min_capacity = bfs(graph, from_, to)
+        if path_exists:
+            for id in path:
+                graph.add_flow(id, min_capacity)
+        else:
+            return flow
+        flow += min_capacity
     return flow
 
 
